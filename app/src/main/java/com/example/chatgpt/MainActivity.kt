e7ef45c3fc67697e5c2d98a30ca409053cd79a98
Minjae <minjaelee@comcom.ai>
2023-07-31 17:21:26 +0900

    WIP: layout

--- app/src/main/java/com/example/chatgpt/MainActivity.kt
+++ app/src/main/java/com/example/chatgpt/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.chatgpt
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,8 +16,24 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.chatgpt.ui.theme.ChatGPTTheme
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var sendBtn: ImageButton;
+    lateinit var resultTv: TextView;
+    lateinit var editText: EditText;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sendBtn = findViewById(R.id.imageButton);
+        resultTv = findViewById(R.id.textView);
+        editText = findViewById(R.id.editTextText);
+
+        sendBtn.setOnClickListener {
+            performAction(editText.text.toString());
+        }
+    }
+
+    fun performAction(vararg input: String){
+        println("performAction");
     }
 }
