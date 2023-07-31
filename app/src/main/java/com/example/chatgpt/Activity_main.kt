c8b8e8673487be3dbf6250dd6edff792d0c8c160
Minjae <minjaelee@comcom.ai>
2023-07-31 16:41:12 +0900

    WIP: send button

---
+++ app/src/main/java/com/example/chatgpt/Activity_main.kt
@@ -0,0 +1,11 @@
+package com.example.chatgpt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
