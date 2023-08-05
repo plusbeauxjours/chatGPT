4784e78bd91f75ed984590c582919f2b5967f6bf
Minjae <minjaelee@comcom.ai>
2023-08-05 22:45:50 +0900

    chatGPT api

--- app/src/main/java/com/example/chatgpt/MainActivity.kt
+++ app/src/main/java/com/example/chatgpt/MainActivity.kt
@@ -13,7 +13,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.example.chatgpt.ui.theme.ChatGPTTheme
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : ComponentActivity() {
 
@@ -30,10 +37,48 @@ class MainActivity : ComponentActivity() {
 
         sendBtn.setOnClickListener {
             performAction(editText.text.toString());
+            editText.text.clear();
         }
+
     }
 
-    fun performAction(vararg input: String){
-        println("performAction");
+    fun performAction(input: String){
+        resultTv.text = input.toString()
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://api.openai.com/v1/chat/completions"
+
+        val jsonObject = JSONObject()
+        val message = JSONObject().apply {
+            put("role", "user")
+            put("content", input)
+        }
+        val messages = JSONArray().put(message)
+
+        jsonObject.put("messages", messages)
+        jsonObject.put("model", "gpt-3.5-turbo")
+        jsonObject.put("temperature", 0)
+        jsonObject.put("top_p", 1)
+        jsonObject.put("frequency_penalty", 0.0)
+        jsonObject.put("presence_penalty", 0.0)
+        val stringRequest = object: JsonObjectRequest(
+            Request.Method.POST, url,jsonObject,
+            Response.Listener<JSONObject> { response ->
+                // Display the first 500 characters of the response string.
+                response.getJSONArray("choices").getJSONObject(0).getString("finish_reason")
+                resultTv.text = "Response is: ${response.getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content")}"
+            },
+            Response.ErrorListener { error ->resultTv.text = messages.toString() }){
+            override fun getHeaders(): MutableMap<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+                // adding headers on below line.
+                params["Content-Type"] = "application/json"
+                params["Authorization"] =
+                    "Bearer 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"
+                return params;
+            }
+        }
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
     }
 }
